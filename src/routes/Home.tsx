8b8070572968b9325970ad3ee4d8cbcb68c034d8
Shinichi Haramizu <shinichi.haramizu@sitecore.com>
2023-08-22 17:18:13 +0900

    Update Home.tsx

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,7 +6,8 @@ const Home: React.FC = () => {
     <div>
       <h1>Welcome to Home Page</h1>
       <p>This is the content of the home page.</p>
-      <Link to="/guestRef/6cfa0297-50bd-4b38-80fc-913a7488b9a8">Test</Link>
+      <Link to="/guestRef/6cfa0297-50bd-4b38-80fc-913a7488b9a8">Profile 1</Link>
+      <Link to="/guestRef/32c0acf1-838a-4925-8ca5-39f0eab4350f">Profile 2</Link>
     </div>
   );
 };
