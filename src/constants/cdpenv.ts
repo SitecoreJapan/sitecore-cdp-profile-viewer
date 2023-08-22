361cdc39d8200d564dbeb25119296a84c22fb2c8
Shinichi Haramizu <113491639+haramizu3@users.noreply.github.com>
2023-08-22 12:37:31 +0900

    add tailwind css

---
+++ src/constants/cdpenv.ts
@@ -0,0 +1,3 @@
+export const baseUrl: string = import.meta.env.VITE_BASE_URL || "";
+export const clientKey: string  = import.meta.env.VITE_CLIENT_KEY || "";
+export const apiToken: string  = import.meta.env.VITE_API_TOKEN || "";
